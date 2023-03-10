553c38e55a2bc3c9774e4e91a7fd93166a99df0a
elaineandreu <73045571+elaineandreu@users.noreply.github.com>
2023-03-10 18:55:22 -0300

    Criação Teste Bug Bank

---
+++ src/main/java/testeBugBank/TesteBugBankApplication.java
@@ -0,0 +1,13 @@
+package testeBugBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteBugBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteBugBankApplication.class, args);
+	}
+
+}
